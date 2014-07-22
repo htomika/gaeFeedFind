d42f071909514c5b4516188ebcf85c50f5d32213
Tamas Hais <htomika@gmail.com>
2014-07-22 23:17:36 +0200

    Changing back to non-rest.

---
+++ constants_sample.py
@@ -0,0 +1,11 @@
+__author__ = 'style'
+
+PUSHER = {
+    'app_id': '12345',
+    'key': 'xxxxxxzxxxxxxxxx',
+    'secret': 'xxxxxxxxxxxxx'
+}
+
+CLIENT_ID = "xxxxxxxxx.apps.googleusercontent.com"
+
+SENTRY = 'https://xxxx:xxxxxx@app.getsentry.com/27675'
